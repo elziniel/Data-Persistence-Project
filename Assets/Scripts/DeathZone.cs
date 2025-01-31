6bca784a8ac3d1a09a8d10fb6248600d9f66fd23
ELZi <elziniel@proton.me>
2025-01-31 22:57:28 +0100

    update on legacy component
    - removed all Text component
    - added Text Mesh Pro component
    - renamed MainManager.cs to GameManager.cs
    - added MainManager.cs : a singleton that takes the player name from the title screen scene to the game scene

--- Assets/Scripts/DeathZone.cs
+++ Assets/Scripts/DeathZone.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class DeathZone : MonoBehaviour
 {
-    public MainManager manager;
+    public GameManager manager;
 
     private void OnCollisionEnter(Collision other)
     {
--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -1,73 +1,25 @@
 using UnityEngine;
-using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 
 public class MainManager : MonoBehaviour
 {
-    public Brick brickPrefab;
-    public int lineCount = 6;
-    public Rigidbody ball;
+    public MainManager instance;
 
-    public Text scoreText;
-    public GameObject gameOverText;
+    public string playerName;
 
-    private bool m_Started = false;
-    private int m_Points;
-
-    private bool m_GameOver = false;
-
-    // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
-        const float step = 0.6f;
-        int perLine = Mathf.FloorToInt(4.0f / step);
-
-        int[] pointCountArray = new[] { 1, 1, 2, 2, 5, 5 };
-        for (int i = 0; i < lineCount; ++i)
+        if (instance != null)
         {
-            for (int x = 0; x < perLine; ++x)
-            {
-                Vector3 position = new Vector3(-1.5f + step * x, 2.5f + i * 0.3f, 0);
-                var brick = Instantiate(brickPrefab, position, Quaternion.identity);
-                brick.pointValue = pointCountArray[i];
-                brick.onDestroyed.AddListener(AddPoint);
-            }
+            Destroy(gameObject);
+            return;
         }
+
+        instance = this;
+        DontDestroyOnLoad(gameObject);
     }
 
-    private void Update()
+    public void SetPlayerName(string name)
     {
-        if (!m_Started)
-        {
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                m_Started = true;
-                float randomDirection = Random.Range(-1.0f, 1.0f);
-                Vector3 forceDir = new Vector3(randomDirection, 1, 0);
-                forceDir.Normalize();
-
-                ball.transform.SetParent(null);
-                ball.AddForce(forceDir * 2.0f, ForceMode.VelocityChange);
-            }
-        }
-        else if (m_GameOver)
-        {
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-            }
-        }
-    }
-
-    void AddPoint(int point)
-    {
-        m_Points += point;
-        scoreText.text = $"Score : {m_Points}";
-    }
-
-    public void GameOver()
-    {
-        m_GameOver = true;
-        gameOverText.SetActive(true);
+        playerName = $"{name}";
     }
 }
