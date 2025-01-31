40f2b0b7e368ce2a11f6b539ae7bbcab9520728c
ELZi <elziniel@proton.me>
2025-01-31 22:00:01 +0100

    title screen and high score menu
    - added a title screen with buttons to start and quit the game
    - added a high score menu with placeholder names and scores
    - fixed some canvas issues not correctly resizing
    - imported TextMesh Pro files

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,34 @@
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuManager : MonoBehaviour
+{
+    public void TitleScreen()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void HighScoreMenu()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit();
+#endif
+    }
+}
